66b3c24a8d2e013e7d28c92b354783976faf748a
roquen4145 <roquen4145@gmail.com>
2022-02-05 23:57:21 +0900

    Rendering unordered list in normal way

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement('div', { style: { color: "blue" } }, React.createElement('h1', null, 'Hello, world!')),
+  React.createElement(
+    'ul',
+    { style: { color: "blue" } },
+    React.createElement('li', null, 'item1'),
+    React.createElement('li', null, 'item2'),
+    React.createElement('li', null, 'item3')
+  ),
   document.getElementById('root')
 );
